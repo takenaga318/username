f2443eb4b753a53ba90754dfafb6c9312de4724b
Audrey Takenaga <takenaga@chapman.edu>
2020-11-13 10:57:15 -0800

    Adds username.sh

--- username.sh
+++ username.sh
@@ -3,11 +3,12 @@
 # Audrey Takenaga
 echo "Enter a Username: "
 read -r  NAME
-while echo "$NAME" | grep -E -v "^[a-z][0-9]{5}$" > /dev/null 2>&1
+while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
-	#need echo lines describing constraints
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You must enter a valid username - between 3-12 characters!"
+	echo "Your username must start with a lowercase letter."
+	echo "And you can only use lowercase letters, digits, and the underscore character"
+	echo "Enter a valid username!"
 	read -r  NAME
 done
 echo "Thank you"
